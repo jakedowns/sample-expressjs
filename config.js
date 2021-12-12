333398e7f2665f3dca361993cec3a79221fffed9
jake <mail@jakedowns.com>
2021-12-12 13:56:49 -0800

    input validation + connectionString on DO

--- config.js
+++ config.js
@@ -1,17 +1,29 @@
 const env = process.env;
 
+const base_db =
+  env.DATABASE_URL && env.DATABASE_URL.length
+    ? {
+        connectionString: `${env.DATABASE_URL}`,
+      }
+    : {
+        host: env.DB_HOST,
+        port: env.DB_PORT,
+        user: env.DB_USER,
+        password: env.DB_PASSWORD,
+        database: env.DB_NAME,
+        // connectionString: `postgres://${env.DB_USER}:${env.DB_PASSWORD}@${env.DB_HOST}:${env.DB_PORT}/${env.DB_NAME}?sslmode=require`
+      };
+
 const config = {
   db: {
-    host: env.DB_HOST,
-    port: env.DB_PORT,
-    user: env.DB_USER,
-    password: env.DB_PASSWORD,
-    database: env.DB_NAME,
-    // connectionString: `postgres://${env.DB_USER}:${env.DB_PASSWORD}@${env.DB_HOST}:${env.DB_PORT}/${env.DB_NAME}?sslmode=require`
-    ssl: {
-        rejectUnauthorized: false
-    }
+    ...{
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+    ...base_db,
   },
+
   listPerPage: env.LIST_PER_PAGE || 10,
 };
 
--- services/tasks.js
+++ services/tasks.js
@@ -17,7 +17,41 @@ async function getMultiple(page = 1) {
   };
 }
 
+// TODO: keyed error bag for input field highlighting
+function validateCreate(task) {
+  let messages = [];
+
+  if (!task) {
+    messages.push('No object is provided')
+  }
+
+  if (!task.text) {
+    messages.push('task.text is empty')
+  }
+
+  if (!task.author) {
+    messages.push('task.author missing')
+  }
+
+  if (task.text && task.text.length > 255) {
+    messages.push('task.text cannot be longer than 255 characters')
+  }
+
+  if (task.author && !parseInt(task.author)) {
+    messages.push('task.author must be integer id')
+  }
+
+  if (messages.length) {
+    let error = new Error(messages.join())
+    error.statusCode = 400;
+
+    throw error;
+  }
+}
+
 async function create(task) {
+  validateCreate(task)
+
   const result = await db.query(
     "INSERT INTO task(text, author) VALUES ($1, $2) RETURNING *",
     [task.text, task.author]
