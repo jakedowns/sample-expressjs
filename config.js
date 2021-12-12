ad4b00b8b86e8af036636f0fa1a09f293cabcebd
jake <mail@jakedowns.com>
2021-12-12 14:47:45 -0800

    attempt to fix db connection on server

--- config.js
+++ config.js
@@ -1,28 +1,38 @@
 const env = process.env;
+const { parse } = require('pg-connection-string')
 
-const base_db =
-  env.DATABASE_URL && env.DATABASE_URL.length
-    ? {
-        connectionString: `${env.DATABASE_URL}`,
-      }
-    : {
-        host: env.DB_HOST,
-        port: env.DB_PORT,
-        user: env.DB_USER,
-        password: env.DB_PASSWORD,
-        database: env.DB_NAME,
-        // connectionString: `postgres://${env.DB_USER}:${env.DB_PASSWORD}@${env.DB_HOST}:${env.DB_PORT}/${env.DB_NAME}?sslmode=require`
-      };
+function getEnvDBConfig(){
+
+  // ENV: DO
+  if(env.DATABASE_URL && env.DATABASE_URL.length){
+    // using connectionString overrides config.ssl
+    // attempting to fix by using config parser
+    // via https://help.heroku.com/MDM23G46/why-am-i-getting-an-error-when-i-upgrade-to-pg-8
+    const config = parse(process.env.DATABASE_URL)
+    config.ssl = {
+      rejectUnauthorized: false
+    }
+    return config;
+  }
+
+  // ENV: local development
+  return {
+    host: env.DB_HOST,
+    port: env.DB_PORT,
+    user: env.DB_USER,
+    password: env.DB_PASSWORD,
+    database: env.DB_NAME,
+    // connectionString: `postgres://${env.DB_USER}:${env.DB_PASSWORD}@${env.DB_HOST}:${env.DB_PORT}/${env.DB_NAME}?sslmode=require`
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
+}
+
+const DB_CONFIG = getEnvDBConfig()
 
 const config = {
-  db: {
-    ...{
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    },
-    ...base_db,
-  },
+  db: DB_CONFIG,
 
   listPerPage: env.LIST_PER_PAGE || 10,
 };
